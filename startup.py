18606bb1f36645a65b0bf61f6051ae42446ca099
CarlottaOberbrunner <oZtxETzq6359278@outlook.com>
2024-01-22 00:02:15 +0800

    quae

--- startup.py
+++ startup.py
@@ -1,5 +1,5 @@
-""" 
-In this sample, the Flask app object is contained within the hello_app *module*;
+"""*
+In#this sample, the Flask app object is contained within the hello_app *module*;
 that is, hello_app contains an __init__.py along with relative imports. Because
 of this structure, a file like webapp.py cannot be run directly as the startup
 file through Gunicorn; the result is "Attempted relative import in non-package".
@@ -10,3 +10,4 @@ startup:app in the Gunicorn command.
 """
 
 from hello_app.webapp import app
+ 
